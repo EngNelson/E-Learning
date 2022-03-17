6e194a0b7550864ddf54337f37949085d7c40a3b
EngNelson <bechem200@gmail.com>
2022-03-17 23:32:14 +0100

    template media queries finish

--- E-Learners/main.js
+++ E-Learners/main.js
@@ -6,7 +6,7 @@ window.addEventListener('scroll', () => {
 
 // show/hide faq answer
 const faq = document.querySelectorAll('.faq');
-faqs.forEach(faq => {
+faq.forEach(faq => {
     faq.addEventListener('click', () => {
         faq.classList.toggle('open');
 
@@ -19,3 +19,23 @@ faqs.forEach(faq => {
         }
     })
 })
+
+// show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// Close  nav menu 
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
